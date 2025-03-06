aa2330bc5d5720669a34d4e89a46c70caab0ce6d
Mert Azko <mertazko61@gmail.com>
2025-03-06 17:13:46 +0300

    Initial commit

---
+++ src/LeapYearCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Yıl Giriniz : ");
+
+        int year = scanner.nextInt();
+        //Leap Year Calculation
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ){
+            System.out.println(year + " bir artık yıldır !");
+
+        } else {
+            System.out.println(year + " bir artık yıldır değildir!");
+
+        }
+    }
+}
